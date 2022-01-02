11ef3b3b3978449e1a32ce9aba0966ca0f0a5c1b
masahirookubo <stmhamachiii@gmail.com>
2022-01-02 17:23:07 +0900

    avatorのexport変更

--- components/post/post-header.tsx
+++ components/post/post-header.tsx
@@ -1,22 +1,18 @@
-import React from 'react';
-import Avatar from '../common/avatar';
-import Date from '../common/date';
+import React from "react";
+import { Avatar } from "../common/avatar";
+import Date from "../common/date";
 
-const PostHeader = ({
-  title,
-  date,
-  author,
-}): JSX.Element => (
+const PostHeader = ({ title, date, author }): JSX.Element => (
   <>
     <div className="flex flex-column my-auto">
+      <div>{author && <Avatar name={author.name} />}</div>
       <div>
-        { author && <Avatar name={author.name} /> }
-      </div>
-      <div>
-        <small className="ml-2"><Date dateString={date} /></small>
+        <small className="ml-2">
+          <Date dateString={date} />
+        </small>
       </div>
     </div>
-    <h1>{ title }</h1>
+    <h1>{title}</h1>
   </>
 );
 
