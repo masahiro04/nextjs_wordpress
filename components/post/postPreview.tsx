3353a73191103a22666eeaf9c0b51422422e0d5c
masahirookubo <stmhamachiii@gmail.com>
2022-07-11 01:01:57 +0900

    modified header

--- components/post/postPreview.tsx
+++ components/post/postPreview.tsx
@@ -2,20 +2,22 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { reistrictCharacters } from '../../utils/helpers';
+import {Date} from '../common/date';
 
 type Props = {
   title: string;
   coverImage: string;
   excerpt: string;
   slug: string;
+  date: string;
 };
 export const PostPreview: React.FC<Props> = (props) => {
-  const { title, coverImage = '/static/images/not_found.png', excerpt, slug } = props;
+  const { title, coverImage = '/static/images/not_found.png', excerpt, slug, date, } = props;
   return (
-    <Link as={`/posts/${slug}`} href={`/posts/${slug}`}>
+    <Link as={`/posts/${slug}`} href={`/posts/${slug}`} className='z-0'>
       <a className='m-4' href={`/posts/${slug}`}>
         <Image
-          className='rounded h-48 w-full'
+          className='rounded h-48 w-full -z-10'
           src={coverImage}
           alt={slug}
           loading='lazy'
@@ -31,6 +33,9 @@ export const PostPreview: React.FC<Props> = (props) => {
           className='break-all text-gray-500'
           dangerouslySetInnerHTML={{ __html: reistrictCharacters(excerpt, 100) }}
         />
+        <div className='mt-2'>
+          <Date dateString={date} />
+        </div>
       </a>
     </Link>
   );
--- lib/constants.ts
+++ lib/constants.ts
@@ -4,7 +4,7 @@ export const PER_PAGE = 12;
 // 固定ページの情報
 // { url: 固定ページのslug, title: 表示するタイトル(個別で表示させたかったので、nextで管理してます) }
 export const PAGES = [
-  { url: 'about', title: 'プロフィール' },
-  { url: 'mvp-programing-document', title: 'MVP教室' },
+  { url: 'posts', title: 'Posts' },
+  // { url: 'mvp-programing-document', title: 'MVP' },
   { url: 'contact', title: 'Contact' }
 ];
