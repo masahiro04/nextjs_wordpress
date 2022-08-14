2bcba86af0fc61dac6d37494a591ad4e6e98de22
masahirookubo <stmhamachiii@gmail.com>
2022-08-14 13:38:08 +0900

    added blog source to footer

--- src/shared/presentation/components/layouts/footer.tsx
+++ src/shared/presentation/components/layouts/footer.tsx
@@ -4,6 +4,29 @@ export const Footer: React.FC = () => (
   <div className='bg-indigo-800 text-white text-center leading-8 w-full py-3'>
     <div className='flex flex-col bg-indigo-800 font-bold'>
       <div>copyright© 2016-{new Date().getFullYear()} Masahiro Okubo</div>
+      <div className='justify-centertext-center'>
+        <a href='https://github.com/masahiro04/nextjs_wordpress' target='_blank' rel='noreferrer'>
+          <div className='flex justify-center'>
+            <div>source code</div>
+            <div className='my-auto'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                className='h-6 ml-2'
+                fill='none'
+                viewBox='0 0 24 24'
+                stroke='currentColor'
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14'
+                />
+              </svg>
+            </div>
+          </div>
+        </a>
+      </div>
     </div>
   </div>
 );
