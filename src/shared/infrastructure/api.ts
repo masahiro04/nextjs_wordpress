e591bece442aba54fd8b0b88b4758d68ffa4f49a
masahirookubo <stmhamachiii@gmail.com>
2023-02-02 23:32:32 +0900

    fix: api request method
    fix: api request method
    
    fix: api request method
    
    fix: api request method
    
    fix: api request method
    
    fix: api request method

--- src/shared/infrastructure/api.ts
+++ src/shared/infrastructure/api.ts
@@ -1,14 +1,24 @@
 export class Api {
   static async get<T>(path: string): Promise<T> {
-    const headers = { 'Content-Type': 'application/json' };
+    const headers = {
+      'Content-Type': 'application/json',
+      Accept: 'application/json, text/plain, */*',
+      'User-Agent': '*'
+    };
     if (!process.env.WORDPRESS_API_URL) {
       throw Error('WORDPRESS_API_URL is not set');
     }
-    return fetch(`${path}`, { headers, method: 'GET' }).then<T>((response) => response.json());
+    return fetch(`${path}`, { headers, method: 'GET' }).then<T>(async (response) => {
+      return response.json();
+    });
   }
 
   static async post<T>(query: string, { variables }: { variables?: Record<string, unknown> } = {}): Promise<T> {
-    const headers = { 'Content-Type': 'application/json' };
+    const headers = {
+      'Content-Type': 'application/json',
+      Accept: 'application/json, text/plain, */*',
+      'User-Agent': '*'
+    };
 
     // WPGraphQL Plugin must be enabled
     return fetch(process.env.WORDPRESS_API_URL ?? '', {
