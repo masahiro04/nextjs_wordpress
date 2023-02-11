d845eadc9c1c8e7f34a1630ecf2d6a6e08ec941b
masahirookubo <stmhamachiii@gmail.com>
2023-02-11 21:20:42 +0900

    feat: deleted unused constant var

--- src/shared/constants/page.ts
+++ src/shared/constants/page.ts
@@ -1,10 +1,1 @@
-// 1ページに表示する記事数
 export const PER_PAGE = 12;
-
-// 固定ページの情報
-// { url: 固定ページのslug, title: 表示するタイトル(個別で表示させたかったので、nextで管理してます) }
-export const PAGES = [
-  { url: 'posts', title: 'Posts' },
-  // { url: 'mvp-programing-document', title: 'MVP' },
-  { url: 'contact', title: 'Contact' }
-];
